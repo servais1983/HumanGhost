11854abd48b2ee4aba1c05f4ad8095489ab953c1
CyberS <stservais0409@gmail.com>
2025-05-16 17:29:54 +0200

    Ajout du module create.py

---
+++ core/create.py
@@ -0,0 +1,5 @@
+def run():
+    print("[*] Création de scénario : Phishing bancaire")
+    print("[+] Sujet : Mise à jour de sécurité")
+    print("[+] Corps : Cliquez ici pour valider votre compte : http://localhost:5000/login")
+    print("[+] Fichier HTML généré : templates/fake_login.html")
