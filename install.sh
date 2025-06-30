4d84bec94beb802c5fef3907f8cde5b9a696cd0a
CyberS <stservais0409@gmail.com>
2025-06-30 21:05:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,17 @@
 echo "[*] Installation de HumanGhost (social engineering) sur Kali..."
 
 # Mise à jour des paquets et installation des prérequis système
+# Ajout des dépendances pour WeasyPrint (génération de PDF)
 sudo apt update
-sudo apt install -y python3-pip python3-venv sendmail
+sudo apt install -y python3-pip python3-venv sendmail build-essential python3-dev python3-cffi libcairo2-dev pango1.0-tools libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
 
-# Création d'un environnement virtuel
+# ... (le reste du script ne change pas) ...
 echo "[*] Création de l'environnement virtuel dans le dossier 'venv'..."
 python3 -m venv venv
 
-# Activation de l'environnement et installation des dépendances Python
 echo "[*] Installation des dépendances Python via pip..."
 source venv/bin/activate
 pip install -r requirements.txt
 
 echo "[+] Installation terminée."
-echo "Pour activer l'environnement, lancez : source venv/bin/activate"
-echo "Ensuite, lancez l'outil avec : python3 humanghost.py run scripts/phishing_exec.yaml"
+# ...
